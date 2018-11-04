8e05e1198a6765ab04bcfbe4afcb53b5bc60f480
Reina Trycha Dani <trycha67@gmail.com>
2018-11-04 15:41:11 +0700

    membuat main class

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class Main {
+    public static void main(String[] args) {
+        TiketVIP v = new TiketVIP();
+        v.setTribun("Tribun VIP");
+        v.setHarga(80000);
+        
+        TiketReguler r = new TiketReguler();
+        r.setTribun("Tribun Reguler");
+        r.setHarga(40000);
+        
+        Pembeli b = new Pembeli();
+        b.setNama("Amara Latifa");
+        b.setAlamat("Jl. Buah Manggis no 3");
+        
+        TiketKonser k = new TiketKonser();
+        k.setVip(v);
+        k.setReguler(r);
+        k.setHarga(80000);
+        k.setJumlah(3);
+        k.setPembeli(b);
+        
+        System.out.println("\tDaftar Tiket");
+        k.infoTiketKonser();
+        System.out.println();
+        System.out.println("\tData Pembeli");
+        b.infoPembeli();
+        System.out.println();
+        System.out.println("Total Harga Tiket: "+k.totalTiket());
+    }
+}
