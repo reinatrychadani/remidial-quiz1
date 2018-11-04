1e8e66557f708b6232abdd04bf6bd268def18ad5
Reina Trycha Dani <trycha67@gmail.com>
2018-11-04 14:24:23 +0700

    membuat atribut dalam class TiketKonser

---
+++ src/TiketKonser.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class TiketKonser {
+    private TiketVIP vip;
+    private TiketReguler reguler;
+    private Pembeli pembeli;
+    
+    
+    
+}
