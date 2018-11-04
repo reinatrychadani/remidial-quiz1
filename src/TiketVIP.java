257c1ffed012e0bbb23e7e3e278ea5e7230418ee
Reina Trycha Dani <trycha67@gmail.com>
2018-11-04 13:20:19 +0700

    membuat konstruktor berparameter dalam class TiketVIP

---
+++ src/TiketVIP.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class TiketVIP {
+    private int harga;
+    private String tribun;
+    
+    //membuat konstruktor tiket vip berparameter
+    public TiketVIP(int harga, String tribun) {
+        this.harga = harga;
+        this.tribun = tribun;
+    }
+    
+    
+}
